133bdac56ea7c8823c7c26341fb7e654c85c10e7
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-24 17:38:14 -0600

    Set current user to store state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 import { movies } from './movies';
 import { hasError } from './hasError';
 import { setLoading } from './setLoading';
-
+import { setUser } from './user';
 
 const rootReducer = combineReducers({
   movies,
   hasError,
-  setLoading
+  setLoading,
+  user: setUser
 });
 
 export default rootReducer;
