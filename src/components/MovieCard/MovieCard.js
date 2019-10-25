320f4e7bef4f2f0fb3898d83e80c61ece93dc348
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-25 13:15:52 -0600

    Add Links to movie cards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -4,34 +4,38 @@ import './MovieCard.scss';
 import { toggleFavorite } from '../../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 
 const MovieCard = movie => {
-  const { title, poster, favorite, toggleFavorite } = movie;
+  const { title, poster, favorite, toggleFavorite, id } = movie;
 
   const handleClick = () => {
     toggleFavorite(title);
   }
 
   return (
-    <div className='MovieCard__div--container'>
-      <img
-        className='MovieCard__img--poster'
-        src={poster}
-        alt='Official movie poster'
-      />
-      <footer className='MovieCard__footer'>
-        <h3 className='MovieCard__h3--title'>{title}</h3>
-        {/* <img
-          className='MovieCard__img--favorite-icon'
-          src={addFavorite}
-          alt='Favorite icon'
-        /> */}
-        <div className='button-container' onClick={handleClick}>
-          <div className={favorite ? 'stick-1-active' : 'stick-1'}></div>
-          <div className={favorite ? 'stick-2-active' : 'stick-2'}></div>
-        </div>
-      </footer>
-    </div>
+
+      <div className='MovieCard__div--container'>
+        <Link to={`/${id}`}>
+          <img
+            className='MovieCard__img--poster'
+            src={poster}
+            alt='Official movie poster'
+          />
+        </Link>
+        <footer className='MovieCard__footer'>
+          <h3 className='MovieCard__h3--title'>{title}</h3>
+          {/* <img
+            className='MovieCard__img--favorite-icon'
+            src={addFavorite}
+            alt='Favorite icon'
+          /> */}
+          <div className='button-container' onClick={handleClick}>
+            <div className={favorite ? 'stick-1-active' : 'stick-1'}></div>
+            <div className={favorite ? 'stick-2-active' : 'stick-2'}></div>
+          </div>
+        </footer>
+      </div>
   );
 };
 
