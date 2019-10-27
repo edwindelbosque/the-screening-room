dbc3bb5c2db6c5a91aebe8b1dd54e86fa83742a6
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-27 12:00:49 -0600

    Remove movies from mapStateToProps in SignUpForm component

--- src/components/SignUpForm/SignUpForm.test.js
+++ src/components/SignUpForm/SignUpForm.test.js
@@ -4,6 +4,18 @@ import { SignUpForm, mapStateToProps, mapDispatchToProps } from './SignUpForm';
 
 describe('SignUpForm', () => {
   let wrapper;
+  const mockEvent = {
+    target: {
+      name: 'name',
+      value: 'Vanessa Randall'
+    }
+  };
+  const mockEvent2 = {
+    target: {
+      name: 'email',
+      value: 'vanessa.randall@doane.edu'
+    }
+  };
 
   beforeEach(() => {
     wrapper = shallow(<SignUpForm />);
@@ -11,5 +23,24 @@ describe('SignUpForm', () => {
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should update it's state property onChange of inputs", () => {
+    wrapper.instance().handleChange(mockEvent);
+    wrapper.instance().handleChange(mockEvent2);
+
+    expect(wrapper.state()).toEqual({
+      name: 'Vanessa Randall',
+      email: 'vanessa.randall@doane.edu',
+      password: '',
+      isLoggedIn: false
+    });
+  });
+
+  it('should call handleClick on submit', () => {
+    wrapper.instance().handleClick = jest.fn();
+    wrapper.find('.Form__button').simulate('click');
+
+    expect(wrapper.instance().handleClick).toHaveBeenCalled();
   });
 });
