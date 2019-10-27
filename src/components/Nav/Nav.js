0c896bc5f99a3a9f2d8bc58f018e1a4c7608f498
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-26 21:05:01 -0600

    Makes Home nav link active on page load movies section

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -20,14 +20,14 @@ export const Nav = ({ logoutCurrentUser, movies, user }) => {
       <header className='Nav'>
         <h1>The Screening Room</h1>
         <div className='Nav__ul'>
-          <NavLink to='/movies' className='Nav__button link-wrapper' activeClassName='nav-active'>
+          <NavLink exact to='/' className='Nav__button link-wrapper' activeClassName='nav-active'>
             <button className='link hover-home'>Home</button>
           </NavLink>
           <NavLink to='/favorites' className='Nav__button link-wrapper' activeClassName='nav-active'>
             <button className='link hover-1'>Favorites</button>
           </NavLink>
           {user.name ? (
-            <NavLink to='/login' className='Nav__button link-wrapper' activeClassName='nav-active'>
+            <NavLink to='/login\' className='Nav__button link-wrapper' activeClassName='nav-active'>
               <button className='link hover-1' onClick={logoutCurrentUser}>
                 Logout
               </button>
