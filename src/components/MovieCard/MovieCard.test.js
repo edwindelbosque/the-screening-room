17a48894fb29feacee1c0bad3808e719d1c377bf
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-29 16:00:51 -0600

    Write and pass some tests in Reducers directory and MovieCards

--- src/components/MovieCard/MovieCard.test.js
+++ src/components/MovieCard/MovieCard.test.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { MovieCard, mapStateToProps, mapDispatchToProps } from './MovieCard';
+import { setFavorites, hasError, toggleFavorite } from '../../actions/index';
 
 describe('MovieCard', () => {
   let wrapper;
   let toggleFavorites = jest.fn();
   let updateFavorites = jest.fn();
+
+  let favorites = [
+    {
+      id: 63,
+      movie_id: 420809,
+      user_id: 1,
+      title: 'Maleficent: Mistress of Evil',
+      poster_path: 'https://image.tmdb.org/t/p/original/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg',
+      release_date: '2019-10-18',
+      vote_average: '7.2',
+      overview: 'Maleficent and her goddaughter Aurora begin to question the complex family ties that bind them as they are pulled in different directions by impending nuptials, unexpected allies, and dark new forces at play.',
+      favorite: true
+    },
+    {
+      id: 63,
+      movie_id: 420809,
+      user_id: 1,
+      title: 'Maleficent: Mistress of Evil',
+      poster_path: 'https://image.tmdb.org/t/p/original/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg',
+      release_date: '2019-10-18',
+      vote_average: '7.2',
+      overview: 'Maleficent and her goddaughter Aurora begin to question the complex family ties that bind them as they are pulled in different directions by impending nuptials, unexpected allies, and dark new forces at play.',
+      favorite: true
+    }
+  ];
+
   let movie = {
     favorite: false,
     genre: (3)[('Crime', 'Drama', 'Thriller')],
@@ -25,8 +52,10 @@ describe('MovieCard', () => {
   beforeEach(() => {
     wrapper = shallow(
       <MovieCard
-        {...movie}
+        favorites={favorites}
+        movie={movie}
         key={movie.id}
+        type={'movies'}
         toggleFavorites={toggleFavorites}
         updateFavorites={updateFavorites}
       />
@@ -36,4 +65,80 @@ describe('MovieCard', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+});
+
+describe('mockStateToProps', () => {
+
+  it('should return an object with the movies data array', () => {
+    const mockState = {
+      movies: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      favorites: [{ id: 1 }, { id: 2 }],
+      user: {name: 'Edwin', id: 33, email: 'edwindelbosquer@gmail.com'}
+    };
+    const expected = {
+      movies: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      favorites: [{ id: 1 }, { id: 2 }],
+      user: {name: 'Edwin', id: 33, email: 'edwindelbosquer@gmail.com'}
+    };
+
+    const mappedProps = mapStateToProps(mockState);
+
+    expect(mappedProps).toEqual(expected);
+  });
+
+  const mockDispatch = jest.fn();
+
+  it('should call dispatch with a setFavorites action when handleClick is called', () => {
+    let favorites = [
+      {
+        id: 63,
+        movie_id: 420809,
+        user_id: 1,
+        title: 'Maleficent: Mistress of Evil',
+        poster_path: 'https://image.tmdb.org/t/p/original/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg',
+        release_date: '2019-10-18',
+        vote_average: '7.2',
+        overview: 'Maleficent and her goddaughter Aurora begin to question the complex family ties that bind them as they are pulled in different directions by impending nuptials, unexpected allies, and dark new forces at play.',
+        favorite: true
+      },
+      {
+        id: 63,
+        movie_id: 420809,
+        user_id: 1,
+        title: 'Maleficent: Mistress of Evil',
+        poster_path: 'https://image.tmdb.org/t/p/original/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg',
+        release_date: '2019-10-18',
+        vote_average: '7.2',
+        overview: 'Maleficent and her goddaughter Aurora begin to question the complex family ties that bind them as they are pulled in different directions by impending nuptials, unexpected allies, and dark new forces at play.',
+        favorite: true
+      }
+    ];
+
+      const actionToDispatch = setFavorites(favorites)
+  
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setFavorites(favorites);
+  
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    });
+
+    it('should call dispatch with a hasError action when handleClick is called', () => {
+      const message = '';
+      const actionToDispatch = hasError(message);
+  
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.hasError(message);
+  
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch with a setUser action when handleClick is called', () => {
+      const actionToDispatch = toggleFavorite('Joker');
+  
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.toggleFavorite('Joker');
+  
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
 });
