c7a9562285275f79aff1ab8b5808d00ffab3f42d
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-16 13:51:20 -0700

    Disable auto-complete in SearchBar input

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -50,6 +50,7 @@ class SearchBar extends Component {
 					name='search'
 					value={this.state.search}
 					onChange={e => this.handleChange(e)}
+					autocomplete="off"
 				/>
 				<NavLink exact to='/search'>
 					<button className='search-button' onClick={this.handleSubmit}>
