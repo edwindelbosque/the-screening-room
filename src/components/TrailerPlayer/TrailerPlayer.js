566a5b6bf76533eff4d77ffb458aca196d8a14d1
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-21 13:39:53 -0700

    Add parameters to embedded youtube video

--- src/components/TrailerPlayer/TrailerPlayer.js
+++ src/components/TrailerPlayer/TrailerPlayer.js
@@ -19,7 +19,7 @@ export const TrailerPlayer = ({ youtubeId }) => {
           width: "100%",
           height: "100%",
         }}
-        src={`https://www.youtube.com/embed/${youtubeId}?controls=0;&autoplay=1`}
+        src={`https://www.youtube.com/embed/${youtubeId}?modestbranding=1&controls=0&autoplay=1&iv_load_policy=3&&showinfo=0`}
         frameBorder="0"
         allowFullScreen
       />
