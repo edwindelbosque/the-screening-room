276887859fad58eb6b17eb283585d8c266ea5430
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-21 11:07:18 -0700

    Update snapshot and apiCalls tests

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -4,33 +4,14 @@ import { Nav } from './Nav';
 
 describe('Nav', () => {
   let wrapper;
-  const favorites = [{id: 2, title: 'Joker'}]
+  let handleCategory = jest.fn();
   let logoutCurrentUser = jest.fn();
-  let user = {
-    name: ''
-  };
-  let movies = [
-    {
-      wallpaper:
-        'https://image.tmdb.org/t/p/original/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg'
-    },
-    {
-      wallpaper:
-        'https://image.tmdb.org/t/p/original/skvI4rYFrKXS73BJxWGH54Omlvv.jpg'
-    },
-    {
-      wallpaper:
-        'https://image.tmdb.org/t/p/original/a6cDxdwaQIFjSkXf7uskg78ZyTq.jpg'
-    }
-  ];
 
   beforeEach(() => {
     wrapper = shallow(
       <Nav 
         logoutCurrentUser={logoutCurrentUser} 
-        movies={movies} 
-        user={user} 
-        favorites={favorites}
+        handleCategory={handleCategory}
       />
     );
   });
